7bf7509b7980439de245ad819fc5f38660fc39b9
Vicey Wang <vicey@live.com>
2017-03-21 09:37:39 +0800

    Add command line args support

--- Program.cs
+++ Program.cs
@@ -2,18 +2,16 @@
 {
     using System;
     using System.Windows.Forms;
+    using Windows;
 
     internal static class Program
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
         [STAThread]
-        private static void Main()
+        private static void Main(string[] args)
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new MainWindow());
+            Application.Run(new MainWindow(args));
         }
     }
 }
