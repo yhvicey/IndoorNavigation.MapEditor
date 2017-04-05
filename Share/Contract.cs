9ff4faf643719179e4a6d596f8ca839acb4256e0
Vicey Wang <vicey@live.com>
2017-04-05 17:03:44 +0800

    Add contract class for contract check

---
+++ Share/Contract.cs
@@ -0,0 +1,18 @@
+﻿namespace IndoorNavigator.MapEditor.Share
+{
+    using System;
+    using System.Linq;
+
+    public static class Contract
+    {
+        public static void EnsureArgsNonNull(params object[] args)
+        {
+            if (args.Any(arg => arg == null)) throw new ArgumentNullException();
+        }
+
+        public static void EnsureValuesNonNull(params object[] values)
+        {
+            if (values.Any(value => value == null)) throw new NullReferenceException();
+        }
+    }
+}
