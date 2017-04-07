94e08ba3445f39cf4d5f4692a04f9a2af957d1c3
Vicey Wang <vicey@live.com>
2017-04-08 02:02:21 +0800

    Add IEnumerable extensions

---
+++ Extensions/IEnumerableExtension.cs
@@ -0,0 +1,25 @@
+﻿namespace IndoorNavigator.MapEditor.Extensions
+{
+    using System;
+    using System.Collections.Generic;
+
+    public static class EnumerableExtension
+    {
+        public static void For<T>(this IEnumerable<T> source, Action<T, int> action)
+        {
+            var index = 0;
+            foreach (var item in source)
+            {
+                action(item, index++);
+            }
+        }
+
+        public static void ForEach<T>(this IEnumerable<T> source, Action<T> action)
+        {
+            foreach (var item in source)
+            {
+                action(item);
+            }
+        }
+    }
+}
