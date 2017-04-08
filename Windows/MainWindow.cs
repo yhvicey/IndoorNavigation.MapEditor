892544056fb46f70911ff044c4566b547ffdf26d
Vicey Wang <vicey@live.com>
2017-04-08 16:25:30 +0800

    Add map view menu and add its callbacks

--- Windows/MainWindow.cs
+++ Windows/MainWindow.cs
@@ -3,7 +3,6 @@
     using System;
     using System.Collections.Generic;
     using System.Drawing;
-    using System.Linq;
     using System.Windows.Forms;
     using Controls;
     using Map;
@@ -16,17 +15,37 @@
     {
         #region Variables
 
+        private int _currentFloorIndex = -1;
+
         private Map _map;
 
-        private readonly MapViewAdapter _mapViewAdapter;
+        private MapViewAdapter _mapViewAdapter;
+
+        private Floor CurrentFloor => _map.Floors[_currentFloorIndex];
 
         #endregion // Variables
 
         #region Initialize functions
 
+        private void InitializeMapElementTreeNode()
+        {
+            MapElementTreeNode.CustomContextMenuStrip = _mapViewMenu;
+        }
+
+        private void InitializeMapViewAdapter()
+        {
+            _mapViewAdapter = new MapViewAdapter(_mapView);
+        }
+
         #endregion // Initialize functions
 
         #region Flush functions
+
+        private void FlushMapStatusLabel()
+        {
+            _mapStatusLable.Text = string.Format(Resources.MapStatusTemplate, _map?.Name ?? "None",
+                _currentFloorIndex == -1 ? "None" : (_currentFloorIndex + 1).ToString());
+        }
 
         public void FlushMapView()
         {
@@ -42,8 +61,10 @@
             try
             {
                 _map = MapParser.Parse(fileName);
-                StatusBarMessage($"Succeed to Load {fileName}.");
                 _mapViewAdapter.Map = _map;
+
+                StatusBarMessage($"Succeed to Load {fileName}.");
+                FlushMapStatusLabel();
                 return true;
             }
             catch (Exception ex)
@@ -55,7 +76,13 @@
 
         private void StatusBarMessage(string message)
         {
-            _statusLabel.Text = message;
+            _messageStatusLabel.Text = message;
+        }
+
+        private void SwitchFloor(int floor)
+        {
+            _currentFloorIndex = floor;
+            FlushMapStatusLabel();
         }
 
         #endregion // Functions
@@ -65,7 +92,10 @@
         public MainWindow(IReadOnlyList<string> args)
         {
             InitializeComponent();
-            _mapViewAdapter = new MapViewAdapter(_mapView);
+
+            InitializeMapElementTreeNode();
+            InitializeMapViewAdapter();
+
             if (args.Count < 1) return;
             LoadMap(args[0]);
         }
@@ -73,6 +103,11 @@
         #endregion // Constructors
 
         #region Event handlers
+
+        private void MainWindowLoad(object sender, EventArgs e)
+        {
+
+        }
 
         private void DesignToolStripItemClicked(object sender, ToolStripItemClickedEventArgs e)
         {
@@ -90,57 +125,185 @@
             _loadFileDialog.Title = Resources.LoadBackgroundDialogTitle;
             _loadFileDialog.Filter = Resources.LoadBackgroundDialogFilter;
             if (_loadFileDialog.ShowDialog(this) != DialogResult.OK) return;
-            if (_floorTabControl.TabCount == 0) return;
             using (var stream = _loadFileDialog.OpenFile())
             {
                 try
                 {
                     var background = Image.FromStream(stream);
-                    var currentFloorTabPage = _floorTabControl.SelectedTab;
-                    currentFloorTabPage.BackgroundImage = background;
-                    currentFloorTabPage.BackgroundImageLayout = ImageLayout.Center;
+                    _designer.BackgroundImage = background;
+                    _designer.BackgroundImageLayout = ImageLayout.Center;
                 }
                 catch (Exception ex)
                 {
-                    MessageBox.Show(this, ex.ToString(), Resources.ErrorDialogTitle);
+                    MessageBox.Show(ex.ToString(), Resources.ErrorDialogTitle);
                 }
             }
         }
 
+        private void MapViewAddFloorMenuItemClick(object sender, EventArgs e)
+        {
+
+        }
+
+        private void MapViewAddEntryNodeMenuItemClick(object sender, EventArgs e)
+        {
+            var wizard = new AddNodeWizard(_map)
+            {
+                Floor = _currentFloorIndex,
+                Type = NodeType.EntryNode
+            };
+            wizard.ShowDialog();
+            if (!wizard.Ready) return;
+            CurrentFloor.AddNode(wizard.MakeNode());
+        }
+
+        private void MapViewAddGuideNodeMenuItemClick(object sender, EventArgs e)
+        {
+            var wizard = new AddNodeWizard(_map)
+            {
+                Floor = _currentFloorIndex,
+                Type = NodeType.GuideNode
+            };
+            wizard.ShowDialog();
+            if (!wizard.Ready) return;
+            CurrentFloor.AddNode(wizard.MakeNode());
+        }
+
+        private void MapViewAddWallNodeMenuItemClick(object sender, EventArgs e)
+        {
+            var wizard = new AddNodeWizard(_map)
+            {
+                Floor = _currentFloorIndex,
+                Type = NodeType.WallNode
+            };
+            if (wizard.ShowDialog() == DialogResult.Cancel) return;
+            if (!wizard.Ready) return;
+            CurrentFloor.AddNode(wizard.MakeNode());
+        }
+
+        private void MapViewAddLinkMenuItemClick(object sender, EventArgs e)
+        {
+            var selectedNode = _mapView.SelectedNode as MapElementTreeNode;
+            if (selectedNode == null) return;
+            _mapView.SelectedNode = selectedNode;
+            var wizard = new AddLinkWizard(_map)
+            {
+                Floor = _currentFloorIndex,
+            };
+            switch (selectedNode.Level)
+            {
+                // Collection node
+                case 2:
+                {
+                    // "3" is magic - stands for links node
+                    if (selectedNode.Index != 3) wizard.StartType = (NodeType)selectedNode.Index;
+                    break;
+                }
+                // Element node
+                case 3:
+                {
+                    if (selectedNode.Parent.Index != 3) wizard.StartType = (NodeType)selectedNode.Parent.Index;
+                    wizard.StartIndex = selectedNode.Index;
+                    break;
+                }
+            }
+            if (wizard.ShowDialog() == DialogResult.Cancel) return;
+            if (!wizard.Ready) return;
+            CurrentFloor.AddLink(wizard.MakeLink());
+        }
+
         private void MapViewNodeMouseClick(object sender, TreeNodeMouseClickEventArgs e)
         {
-            var treeNode = e.Node as MapElementTreeNode;
-            if (treeNode == null) return;
-            _mapView.SelectedNode = treeNode;
+            var selectedNode = e.Node as MapElementTreeNode;
+            if (selectedNode == null) return;
+            _mapView.SelectedNode = selectedNode;
+            if (selectedNode.Level == 1) SwitchFloor(selectedNode.Index);
             switch (e.Button)
             {
                 case MouseButtons.Left:
                 {
-                    _propertyGrid.SelectedObject = treeNode.MapElement;
+                    _propertyGrid.SelectedObject = selectedNode.MapElement;
                     return;
                 }
                 case MouseButtons.Right:
                 {
-                    switch (treeNode.MapElement)
+                    _mapViewAddMenuItem.Visible = false;
+                    _mapViewAddFloorMenuItem.Visible = false;
+                    _mapViewAddEntryNodeMenuItem.Visible = false;
+                    _mapViewAddGuideNodeMenuItem.Visible = false;
+                    _mapViewAddWallNodeMenuItem.Visible = false;
+                    _mapViewAddLinkMenuItem.Visible = false;
+                    switch (e.Node.Level)
                     {
-                        case Map map:
+                        // Root node
+                        case 0:
                         {
-                            _mapView.ContextMenuStrip = _mapTreeNodeMenu;
+                            _mapViewAddMenuItem.Visible = true;
+                            _mapViewAddFloorMenuItem.Visible = true;
                             return;
                         }
-                        case Floor floor:
+                        // Floor node
+                        case 1:
                         {
-                            _mapView.ContextMenuStrip = _floorTreeNodeMenu;
+                            _mapViewAddMenuItem.Visible = true;
+                            _mapViewAddEntryNodeMenuItem.Visible = true;
+                            _mapViewAddGuideNodeMenuItem.Visible = true;
+                            _mapViewAddWallNodeMenuItem.Visible = true;
+                            _mapViewAddLinkMenuItem.Visible = true;
                             return;
                         }
-                        case NodeBase node:
+                        // Collection node
+                        case 2:
                         {
-                            _mapView.ContextMenuStrip = _nodeTreeNodeMenu;
+                            switch (selectedNode.Index)
+                            {
+                                // Entry nodes
+                                case 0:
+                                {
+                                    _mapViewAddMenuItem.Visible = true;
+                                    _mapViewAddEntryNodeMenuItem.Visible = true;
+                                    _mapViewAddLinkMenuItem.Visible = true;
+                                    return;
+                                }
+                                // Guide nodes
+                                case 1:
+                                {
+                                    _mapViewAddMenuItem.Visible = true;
+                                    _mapViewAddGuideNodeMenuItem.Visible = true;
+                                    _mapViewAddLinkMenuItem.Visible = true;
+                                    return;
+                                }
+                                // Wall nodes
+                                case 2:
+                                {
+                                    _mapViewAddMenuItem.Visible = true;
+                                    _mapViewAddWallNodeMenuItem.Visible = true;
+                                    _mapViewAddLinkMenuItem.Visible = true;
+                                    return;
+                                }
+                                // Links
+                                case 3:
+                                {
+                                    _mapViewAddMenuItem.Visible = true;
+                                    _mapViewAddLinkMenuItem.Visible = true;
+                                    return;
+                                }
+                                default:
+                                {
+                                    return;
+                                }
+                            }
+                        }
+                        // Element node
+                        case 3:
+                        {
+                            if (selectedNode.MapElement is Link) return;
+                            _mapViewAddMenuItem.Visible = true;
+                            _mapViewAddLinkMenuItem.Visible = true;
                             return;
                         }
                         default:
                         {
-                            _mapView.ContextMenuStrip = null;
                             return;
                         }
                     }
@@ -159,43 +322,9 @@
 
         private void RemoveBackgroundMenuItemClick(object sender, EventArgs e)
         {
-            if (_floorTabControl.TabCount == 0) return;
-            _floorTabControl.SelectedTab.BackgroundImage = null;
+            _designer.BackgroundImage = null;
         }
 
         #endregion // Event handlers
-
-        private void magicToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            var wizard = new AddNodeWizard(_map);
-            if (wizard.ShowDialog() != DialogResult.Yes) return;
-            var floorIndex = wizard.Floor;
-            var floor = _map.Floors[floorIndex];
-            NodeBase node;
-            switch (wizard.Type)
-            {
-                case NodeType.EntryNode:
-                {
-                    node = new EntryNode(floor, wizard.X, wizard.Y, wizard.NodeName, wizard.Prev, wizard.Next);
-                    break;
-                }
-                case NodeType.GuideNode:
-                {
-                    node = new GuideNode(floor, wizard.X, wizard.Y, wizard.NodeName);
-                    break;
-                }
-                case NodeType.WallNode:
-                {
-                    node = new WallNode(floor, wizard.X, wizard.Y);
-                    break;
-                }
-                default:
-                {
-                    return;
-                }
-            }
-            floor.AddNode(node);
-            _mapViewAdapter.AddNode(floorIndex, node);
-        }
     }
 }
